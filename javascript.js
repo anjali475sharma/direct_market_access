f3502159e537789adf187c81d72774f70af62a66
anjali475sharma <475anjalisharma@gmail.com>
2024-09-17 19:33:03 +0530

    SIH Project

---
+++ javascript.js
@@ -0,0 +1,4 @@
+{/* <img src="https://media.istockphoto.com/id/1152417176/photo/indian-rural-farmer-using-laptop.jpg?s=2048x2048&w=is&k=20&c=HW8KIxuUt_69shL1rmuPdPYv5I-oEmblXzG8Xs3wsck="> */}
+
+
+// <!-- <script src="https://cdn.tailwindcss.com"></script> -->
